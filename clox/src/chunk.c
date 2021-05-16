2271b19cf33b26b694162f2a87018a508b97ee6c
Andrew Dibble <andrew@dibble.codes>
2021-05-16 18:41:50 +0200

    More efficient line tracking

--- clox/src/chunk.c
+++ clox/src/chunk.c
@@ -7,13 +7,15 @@ void initChunk(Chunk* chunk) {
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
+  chunk->lineCount = 0;
+  chunk->lineCapacity = 0;
   chunk->lines = NULL;
   initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk* chunk) {
   FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
-  FREE_ARRAY(uint8_t, chunk->lines, chunk->capacity);
+  FREE_ARRAY(LineStart, chunk->lines, chunk->capacity);
   freeValueArray(&chunk->constants);
   initChunk(chunk);
 }
@@ -24,12 +26,25 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
     chunk->capacity = GROW_CAPACITY(oldCapacity);
     chunk->code =
         GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
-    chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
   }
 
   chunk->code[chunk->count] = byte;
-  chunk->lines[chunk->count] = line;
   chunk->count++;
+
+  if (chunk->lineCount > 0 && chunk->lines[chunk->lineCount - 1].line == line) {
+    return;
+  }
+
+  if (chunk->lineCapacity < chunk->lineCount + 1) {
+    int oldCapacity = chunk->lineCapacity;
+    chunk->lineCapacity = GROW_CAPACITY(oldCapacity);
+    chunk->lines =
+        GROW_ARRAY(LineStart, chunk->lines, oldCapacity, chunk->lineCapacity);
+  }
+
+  LineStart* lineStart = &chunk->lines[chunk->lineCount++];
+  lineStart->offset = chunk->count - 1;
+  lineStart->line = line;
 }
 
 int addConstant(Chunk* chunk, Value value) {
@@ -47,5 +62,23 @@ void writeConstant(Chunk* chunk, Value value, int line) {
   } else {
     writeChunk(chunk, OP_CONSTANT, line);
     writeChunk(chunk, index, line);
+  }
+}
+
+int getLine(Chunk* chunk, int instruction) {
+  size_t start = 0;
+  size_t end = chunk->lineCount - 1;
+
+  while (true) {
+    size_t mid = (start + end) / 2;
+    LineStart* line = &chunk->lines[mid];
+    if (instruction < line->offset) {
+      end = mid - 1;
+    } else if (mid == chunk->lineCount - 1 ||
+               instruction < chunk->lines[mid + 1].offset) {
+      return line->line;
+    } else {
+      start = mid + 1;
+    }
   }
 }
--- clox/src/main.c
+++ clox/src/main.c
@@ -9,8 +9,9 @@ int main(int argc, const char* argv[]) {
   // int constant = addConstant(&chunk, 1.2);
   // writeChunk(&chunk, OP_CONSTANT, 123);
   // writeChunk(&chunk, constant, 123);
-  for (size_t i = 0; i < 300; i++) {
-    writeConstant(&chunk, i, 123);
+  for (size_t i = 0, line = 0; i < 300; i++) {
+    if (i % 3 == 0) line++;
+    writeConstant(&chunk, i, line);
   }
 
   writeChunk(&chunk, OP_RETURN, 123);
