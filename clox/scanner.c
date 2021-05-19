88add6228f7c6d8e2b6e026bb5a269e0e8c93deb
Andrew Dibble <andrew@dibble.codes>
2021-05-19 11:43:41 +0200

    Add more constants

--- clox/scanner.c
+++ clox/scanner.c
@@ -94,12 +94,10 @@ static void skipWhitespace() {
   }
 }
 
-static TokenType checkKeyword(
-    int start,
-    int length,
-    const char* rest,
-    TokenType type
-) {
+static TokenType checkKeyword(int start,
+                              int length,
+                              const char* rest,
+                              TokenType type) {
   if (scanner.current - scanner.start == start + length &&
       memcmp(scanner.start + start, rest, length) == 0) {
     return type;
