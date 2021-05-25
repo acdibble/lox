9e346189bfcb568953715c04f5cd6c31868cdbd3
Andrew Dibble <andrew@dibble.codes>
2021-05-25 20:37:46 +0200

    Add bound methods

--- clox/vm.c
+++ clox/vm.c
@@ -230,6 +230,13 @@ static void closeUpvalues(Value* last) {
   }
 }
 
+static void defineMethod(ObjString* name) {
+  Value method = peek(0);
+  ObjClass* klass = AS_CLASS(peek(1));
+  tableSet(&klass->methods, name, method);
+  pop();
+}
+
 static bool isFalsy(Value value) {
   return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
 }
@@ -498,6 +505,9 @@ static InterpretResult run() {
       }
       case OP_CLASS:
         push(OBJ_VAL(newClass(READ_STRING())));
+        break;
+      case OP_METHOD:
+        defineMethod(READ_STRING());
         break;
     }
   }
