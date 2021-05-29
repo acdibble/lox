fff0742af716c8a965266b02cf7456eea25102ef
Andrew Dibble <andrew@dibble.codes>
2021-05-29 19:12:21 +0200

    Finish scanning

--- src/scanner.rs
+++ src/scanner.rs
@@ -44,6 +44,7 @@ pub enum TokenKind {
   True,
   Var,
   While,
+
   Error,
 }
 
@@ -74,7 +75,7 @@ impl<'a> Scanner<'a> {
     self.iter.next()
   }
 
-  fn match_next(&mut self, expected: char) -> bool {
+  fn match_current(&mut self, expected: char) -> bool {
     match self.iter.next_if(|&(_, c)| c == expected) {
       Some(_) => true,
       None => false,
@@ -82,23 +83,46 @@ impl<'a> Scanner<'a> {
   }
 
   fn skip_whitespace(&mut self) {
-    while let Some((_, c)) = self.iter.next_if(|&(_, c)| c.is_whitespace()) {
-      if c == '\n' {
-        self.lines += 1;
+    while let Some((_, c)) = self.iter.peek() {
+      match *c {
+        ' ' | '\r' | '\t' => {
+          self.advance();
+        }
+        '\n' => {
+          self.lines += 1;
+          self.advance();
+        }
+        '/' => {
+          if let Some((_, c)) = self.peek_next() {
+            if c == '/' {
+              while self.iter.next_if(|&(_, c)| c != '\n').is_some() {}
+            } else {
+              return;
+            }
+          } else {
+            return;
+          }
+        }
+        _ => return,
       }
     }
   }
 
-  fn make_token(&mut self, kind: TokenKind) -> Token<'a> {
+  fn get_lexeme(&mut self) -> &'a str {
     let end = if let Some((number, _)) = self.iter.peek() {
       *number
     } else {
-      self.start + 1
+      self.source.len()
     };
+
+    &self.source[self.start..end]
+  }
+
+  fn make_token(&mut self, kind: TokenKind) -> Token<'a> {
     Token {
       kind: kind,
       line: self.lines,
-      lexeme: &self.source[self.start..end],
+      lexeme: self.get_lexeme(),
     }
   }
 
@@ -108,6 +132,86 @@ impl<'a> Scanner<'a> {
       line: self.lines,
       lexeme: message,
     }
+  }
+
+  fn peek_next(&mut self) -> Option<(usize, char)> {
+    if let Some((n, _)) = self.iter.peek() {
+      if let Some(byte) = self.source.as_bytes().get(n + 1) {
+        return Some((n + 1, *byte as char));
+      }
+    }
+
+    None
+  }
+
+  fn string(&mut self) -> Option<Token<'a>> {
+    while let Some((_, c)) = self.iter.next_if(|&(_, c)| c != '"') {
+      if c == '\n' {
+        self.lines += 1;
+      }
+    }
+
+    let token = if self.match_current('"') {
+      self.make_token(TokenKind::String)
+    } else {
+      self.make_error_token("Unterminated string.")
+    };
+
+    Some(token)
+  }
+
+  fn number(&mut self) -> Option<Token<'a>> {
+    while self.iter.next_if(|&(_, c)| c.is_digit(10)).is_some() {}
+
+    // Look for a fractional part.
+    if let Some((_, c)) = self.iter.peek() {
+      if *c == '.' {
+        if let Some((_, next)) = self.peek_next() {
+          if next.is_digit(10) {
+            // Consume the ".".
+            self.advance();
+            while self.iter.next_if(|&(_, c)| c.is_digit(10)).is_some() {}
+          }
+        }
+      }
+    }
+
+    Some(self.make_token(TokenKind::Number))
+  }
+
+  fn identifier(&mut self) -> Option<Token<'a>> {
+    while self
+      .iter
+      .next_if(|&(_, c)| c.is_ascii_alphabetic() || c == '_')
+      .is_some()
+    {}
+
+    let lexeme = self.get_lexeme();
+    let kind = match lexeme {
+      "and" => TokenKind::And,
+      "class" => TokenKind::Class,
+      "else" => TokenKind::Else,
+      "false" => TokenKind::False,
+      "for" => TokenKind::For,
+      "fun" => TokenKind::Fun,
+      "if" => TokenKind::If,
+      "nil" => TokenKind::Nil,
+      "or" => TokenKind::Or,
+      "print" => TokenKind::Print,
+      "return" => TokenKind::Return,
+      "super" => TokenKind::Super,
+      "this" => TokenKind::This,
+      "true" => TokenKind::True,
+      "var" => TokenKind::Var,
+      "while" => TokenKind::While,
+      _ => TokenKind::Identifier,
+    };
+
+    Some(Token {
+      kind: kind,
+      lexeme: lexeme,
+      line: self.lines,
+    })
   }
 
   pub fn scan_token(&mut self) -> Option<Token<'a>> {
@@ -133,43 +237,38 @@ impl<'a> Scanner<'a> {
       '-' => self.make_token(TokenKind::Minus),
       '+' => self.make_token(TokenKind::Plus),
       '*' => self.make_token(TokenKind::Star),
-      '/' => {
-        // handle comments here
-        if self.match_next('/') {
-          while self.iter.next_if(|&(_, c)| c != '\n').is_some() {}
-          return self.scan_token();
-        }
-
-        self.make_token(TokenKind::Slash)
-      }
+      '/' => self.make_token(TokenKind::Slash),
       '!' => {
-        if self.match_next('=') {
+        if self.match_current('=') {
           self.make_token(TokenKind::BangEqual)
         } else {
           self.make_token(TokenKind::Bang)
         }
       }
       '=' => {
-        if self.match_next('=') {
+        if self.match_current('=') {
           self.make_token(TokenKind::EqualEqual)
         } else {
           self.make_token(TokenKind::Equal)
         }
       }
       '<' => {
-        if self.match_next('=') {
+        if self.match_current('=') {
           self.make_token(TokenKind::LessEqual)
         } else {
           self.make_token(TokenKind::Less)
         }
       }
       '>' => {
-        if self.match_next('=') {
+        if self.match_current('=') {
           self.make_token(TokenKind::GreaterEqual)
         } else {
           self.make_token(TokenKind::Greater)
         }
       }
+      '"' => return self.string(),
+      '0'..='9' => return self.number(),
+      'a'..='z' | 'A'..='Z' | '_' => return self.identifier(),
       _ => self.make_error_token("Unexpected character."),
     };
 
