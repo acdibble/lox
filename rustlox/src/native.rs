9bb2087d8918318d135ec0a128d123aeefc9189e
Andrew Dibble <andrew@dibble.codes>
2021-06-15 22:14:11 +0200

    Add Rust TIL

--- rustlox/src/native.rs
+++ rustlox/src/native.rs
@@ -1,13 +1,9 @@
 use crate::value::*;
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::result::Result;
+use std::time::{SystemTime, SystemTimeError, UNIX_EPOCH};
 
 pub type Function = fn(args: &[Value]) -> Value;
 
-pub fn clock(_args: &[Value]) -> Value {
-    let start = SystemTime::now();
-    let since_the_epoch = start
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards")
-        .as_secs_f64();
-    Value::Number(since_the_epoch)
+pub fn clock(_args: &[Value]) -> Result<f64, SystemTimeError> {
+    Ok(SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs_f64())
 }
